7db86f166e8eefb7e8c850e1a57cbab1fa513b8d
Utkarsh3007 <73110738+Utkarsh3007@users.noreply.github.com>
2020-12-06 17:01:50 +0530

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,21 @@
+class Block2 {
+    constructor(x, y, width, height) {
+        var option = {
+        isStatic:false,
+        
+        }
+        this.body = Bodies.rectangle(x, y, width, height, option);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body)
+    }
+    display() {
+        var pos = this.body.position;
+    
+        rectMode(CENTER)
+        fill("#FFC0CB")
+        stroke("black")
+        strokeWeight(2.5)
+        rect(pos.x,pos.y, this.width, this.height)                           
+    }
+}
